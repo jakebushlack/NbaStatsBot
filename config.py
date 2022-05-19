ba5e49c575506ee20d0d5098a3795f2c2a8a2057
jakebushlack <58116109+jakebushlack@users.noreply.github.com>
2022-05-18 22:30:21 -0600

    store static variables in config file

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 urls = {
     'per_game': 'https://www.basketball-reference.com/leagues/NBA_2022_per_game.html',
-    'totals': 'https://www.basketball-reference.com/leagues/NBA_2022_per_game.html',
-    'per_36_min': 'https://www.basketball-reference.com/leagues/NBA_2022_per_game.html'
+    'totals': 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html',
+    'per_36_min': 'https://www.basketball-reference.com/leagues/NBA_2022_per_minute.html',
+    'per_100_poss': 'https://www.basketball-reference.com/leagues/NBA_2022_per_poss.html'
 }
 
 test_content = 'test_html.html'

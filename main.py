d5b842c264707bf8c06f9324dae1f8bbde007ef2
jakebushlack <58116109+jakebushlack@users.noreply.github.com>
2022-05-18 22:31:04 -0600

    simple void executable

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from dataScrape import get_web_content, get_table, save_table_to_csv
+from dataService import get_player_data
+from config import urls
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+get_web_content()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+get_player_data("stat_files/per_game_stats")

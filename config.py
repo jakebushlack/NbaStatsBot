fb54ff9de58e34a95869d75e098623dddbbc46a0
jakebushlack <58116109+jakebushlack@users.noreply.github.com>
2022-05-21 14:48:53 -0600

    fix file names

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 urls = {
     'per_game': 'https://www.basketball-reference.com/leagues/NBA_2022_per_game.html',
     'totals': 'https://www.basketball-reference.com/leagues/NBA_2022_totals.html',
-    'per_36_min': 'https://www.basketball-reference.com/leagues/NBA_2022_per_minute.html',
-    'per_100_poss': 'https://www.basketball-reference.com/leagues/NBA_2022_per_poss.html'
+    'per_minute': 'https://www.basketball-reference.com/leagues/NBA_2022_per_minute.html',
+    'per_poss': 'https://www.basketball-reference.com/leagues/NBA_2022_per_poss.html'
 }
 
 test_content = 'test_html.html'

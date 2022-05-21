237b05f85f49d2fc9832e645a8d2de10a1450de3
jakebushlack <58116109+jakebushlack@users.noreply.github.com>
2022-05-21 14:49:55 -0600

    wip new get/join

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dataScrape import get_web_content, get_table, save_table_to_csv
-from dataService import get_player_data
+from dataService import get_player_data, get_player_data
 from config import urls
 
 get_web_content()
 
-get_player_data("stat_files/per_game_stats")
+new_get_player_data({})

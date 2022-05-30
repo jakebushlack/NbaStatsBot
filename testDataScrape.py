03227f30a2aeae90a49fd8ddb4b6209cce08160d
jakebushlack <58116109+jakebushlack@users.noreply.github.com>
2022-05-29 21:49:25 -0600

    fix tests

---
+++ testDataScrape.py
@@ -0,0 +1,27 @@
+import csv
+import config
+from bs4 import BeautifulSoup, element
+from unittest import TestCase
+
+
+class DataScrapeTest(TestCase):
+    def test_get_web_content(self):
+        from dataScrape import get_web_content
+        self.assertEqual(get_web_content(config.urls['totals']).content, 200)
+
+    def test_get_table(self):
+        from dataScrape import get_table_from_html
+        with open(config.test_content['test_html'], "r", encoding="utf-8") as test_html:
+            table = get_table_from_html(BeautifulSoup(test_html.read(), 'html.parser'))
+            self.assertGreater(len(table), 0)
+            self.assertTrue(type(table) == element.Tag)
+
+    def test_save_csv(self):
+        from dataScrape import get_table_from_html, save_table_to_csv
+        with open(config.test_content['test_html'], "r", encoding="utf-8") as test_html:
+            table = get_table_from_html(BeautifulSoup(test_html.read(), 'html.parser'))
+            save_table_to_csv(table)
+            with open('stat_files/test_table.csv', "r", encoding="utf-8") as test_csv:
+                csv_reader = list(csv.reader(test_csv, delimiter=','))
+                self.assertGreater(len(csv_reader), 0)
+
--- testDataService.py
+++ testDataService.py
@@ -1,13 +1,32 @@
+import csv
 from unittest import TestCase
+
+import config
 from config import test_content
 
+test_file = test_content['test_csv']
 
-test_file = open(test_content)
 
 class TestFindStatsTable(TestCase):
-    def test_get_stat_categories(self):
-        from dataScrape import get_web_content
-        from config import urls
-        self.assertEqual(get_web_content(urls['per_game']).status_code, 200)
+    def test_find_stats_table(self):
+        from dataService import get_player_data
+        self.assertGreater(len(get_player_data({})), 0)
 
+    def test_remove_duplicate_stat_categories(self):
+        from dataService import get_player_data
+        stats_count = 0
+        for file_name in config.urls:
+            with open(f'stat_files/{file_name}_stats.csv', encoding='UTF-8') as stats_file:
+                csv_reader = list(csv.reader(stats_file, delimiter=','))
+                stats_count += len(csv_reader[0])
+        self.assertLess(len(dir(get_player_data({})['Marcus Smart'])), stats_count)
+
+    def test_remove_duplicate_stat_categories(self):
+        from dataService import get_player_data
+        stats_count = 0
+        for file_name in config.urls:
+            with open(f'stat_files/{file_name}_stats.csv', encoding='UTF-8') as stats_file:
+                csv_reader = list(csv.reader(stats_file, delimiter=','))
+                stats_count += len(csv_reader[0])
+        self.assertLess(len(dir(get_player_data({})['Marcus Smart'])), stats_count)
 
